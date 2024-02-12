953b52f37afe9e8efe5e0550fbd1e8a7151d1937
MasamRehman <masamrehman6@gmail.com>
2024-02-12 19:24:27 +0500

    latest_code
    latest_code

--- lib/provider/weather_provider.dart
+++ lib/provider/weather_provider.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 
 import '../models/weather_model.dart';
@@ -18,15 +18,9 @@ class WeatherProvider extends ChangeNotifier {
   Locale? _appLocale;
   Locale? get applocale => _appLocale;
   void changeLanguage(Locale type) async {
-    SharedPreferences sp = await SharedPreferences.getInstance();
+    
     _appLocale = type;
-    if (type == Locale("en")) {
-      await sp.setString("language_code", "en");
-       notifyListeners();
-    } else {
-      await sp.setString("langugae_code", "ar");
-       notifyListeners();
-    }
+    
     notifyListeners();
   }
 
